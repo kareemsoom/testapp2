489597bd82d1766ef701556ffb0a06be4cdc2296
kareemsoom <kareemsoom@yahoo.com>
2019-02-21 14:11:34 +0100

    Add project files.

---
+++ TestApplication1/Program.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication1
+{
+    class Program
+    {
+           
+        static void Main(string[] args)
+        {
+
+            TestClass1 c1 = new TestClass1();
+            TestClass1 c2 = new TestClass1();
+
+            var loginPage = new LoginPage();
+            loginPage.UserName("farhan");
+            //Console.WriteLine( loginPage.GetType());
+
+            c1.SetValue(30);
+            c1.GetValue();
+
+            c2.SetValue(40);
+            c2.GetValue();
+
+            c1.SetValue(50);
+            c1.GetValue();
+            c1.GetValue();
+
+
+            //c1.Testcase1("PASSED");
+            //c1.Testcase2("FAILED");
+
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        public class LoginPage
+        {
+            public void UserName(String hameed)
+            {
+                Console.WriteLine(hameed) ;
+            }
+                
+        }
+
+        public class UserListPage
+        {
+            public string ListOfUsers { get; set; }
+        }
+
+    }
+}
---
+++ TestApplication1/TestClass1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication1
+{
+    class TestClass1
+    {
+        int i = 0;
+        public void GetValue()
+        {
+            Console.WriteLine("value for i: " + i );
+        }
+
+        public void SetValue(int value)
+        {
+            i = value;
+        }
+
+        public void Testcase1(String result)
+        {
+            Console.WriteLine(" testcase1: " + result);
+        }
+
+
+        public void Testcase2(String result)
+        {
+            Console.WriteLine(" testcase2: " + result);
+        }
+    }
+}
